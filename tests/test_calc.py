ac5fb700dbdfd9a5e4d614d9c4d7226b44d12e8a
tokunaga-rio <46846774+tokunaga-rio@users.noreply.github.com>
2022-02-03 21:47:52 +0900

    Update test_calc.py

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -3,6 +3,6 @@ import calc
 
 class TestCalc(unittest.TestCase):
     def test_plus(self):
-        self.assertEqual(calc.plus(3,5), 8)
+        self.assertEqual(calc.plus(2,7), 10)
     def test_minus(self):
         self.assertEqual(calc.minus(10,3), 7)
